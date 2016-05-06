42f9393473c6d8ec3dc58162b46aadfd3dbcebf2
Maxpal92 <projet2a@imac-cours-6.local>
2016-05-06 11:18:19 +0200

    Déplacement des fonctions du Plateau dans la classe Contrôleur

--- start/Controleur.swift
+++ start/Controleur.swift
@@ -10,4 +10,43 @@ import Foundation
 
 class Controleur {
     
+    var plateau = Plateau()
+    
+    func update(){
+        
+        plateau.table  = [[Int]](count:plateau.lignes, repeatedValue:[Int](count: plateau.colonnes, repeatedValue:-1))
+        
+        for i in 0..<plateau.cars.count
+        {
+            if(plateau.cars[i].isVertical)
+            {
+                for j in 0..<plateau.cars[i].length
+                {
+                    plateau.table[plateau.cars[i].x+j][plateau.cars[i].y] = plateau.cars[i].Id
+                }
+            }
+            else
+            {
+                for k in 0..<plateau.cars[i].length
+                {
+                    plateau.table[plateau.cars[i].x][plateau.cars[i].y+k] = plateau.cars[i].Id
+                }
+            }
+        }
+    }
+    
+    func afficheTab()->String{
+        update()
+        abc=""
+        for i in 0..<plateau.lignes {
+            for j in 0..<plateau.colonnes {
+                abc += " '\(plateau.table[i][j]) '"
+            }
+            abc += "\n"
+        }
+        return abc
+        
+    }
+
+    
 }
--- start/Plateau.swift
+++ start/Plateau.swift
@@ -25,42 +25,7 @@ class Plateau {
     }
 
 
-    func update(){
         
-            table  = [[Int]](count:lignes, repeatedValue:[Int](count: colonnes, repeatedValue:-1))
-        
-            for i in 0..<cars.count
-            {
-                if(cars[i].isVertical)
-                {
-                    for j in 0..<cars[i].length
-                    {
-                        table[cars[i].x+j][cars[i].y] = cars[i].Id
-                    }
-                }
-                else
-                {
-                    for k in 0..<cars[i].length
-                    {
-                        table[cars[i].x][cars[i].y+k] = cars[i].Id
-                    }
-                }
-            }
-    }
-    
-    func afficheTab()->String{
-        update()
-        abc=""
-        for i in 0..<lignes {
-            for j in 0..<colonnes {
-                abc += " '\(table[i][j]) '"
-            }
-            abc += "\n"
-        }
-        return abc
-        
-    }
-    
     
     
     }
