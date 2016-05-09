e8994b7dae7d8e89a5368f1f65c1d7341aec172a
Maxpal92 <projet2a@imac-cours-6.local>
2016-05-09 09:33:48 +0200

    test

---
+++ start/CarView.swift
@@ -0,0 +1,19 @@
+//
+//  CarView.swift
+//  start
+//
+//  Created by Projet 2A on 09/05/2016.
+//  Copyright © 2016 Projet 2A. All rights reserved.
+//
+
+import Cocoa
+
+class CarView: NSView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
